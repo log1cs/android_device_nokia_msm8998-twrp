ef2d6c67d31b22661c434d26bd20b870e0d0ad98
Tuan Anh <tuan73176@gmail.com>
2023-09-05 10:59:04 +0700

    nokia8998: lunch fix

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_cheeseburger_dumpling.mk
+    $(LOCAL_DIR)/twrp_nokia8998.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_cheeseburger_dumpling-eng \
-    twrp_cheeseburger_dumpling-userdebug
+    twrp_nokia8998-eng \
+    twrp_nokia8998-userdebug
