66b6d7eebd0729720ce364359cb0271bf7974c1c
Tuan Anh <tuan73176@gmail.com>
2023-12-05 00:10:32 +0700

    NLA: Assert NB1 & NLA
    Change-Id: I3b3daa050f820d3bf1c0a95c383c3ac8de52571d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,9 @@
 
 DEVICE_PATH := device/nokia/NLA
 
+# Assertion
+TARGET_OTA_ASSERT_DEVICE := NLA,NB1
+
 # Include device common makefiles
 include device/nokia/msm8998-common/BoardConfigCommon.mk
 
